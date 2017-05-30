2249c4d2fe094f555fdc34497813845214a7fed2
Cedric Haase <cedric@sineband.de>
2017-05-30 13:11:53 +0200

    Implement REST API

--- app.py
+++ app.py
@@ -2,40 +2,88 @@ import os
 from subprocess import Popen
 
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 PROGRAM_PROCESS = None
+PROGRAM_PATH = os.getcwd() + '/programs/'
+
+
+def get_running_program():
+    """
+    :return: The name of the program that is currently running 
+    """
+    return PROGRAM_PROCESS.args[1].split('/')[-1][:-3] if PROGRAM_PROCESS else ""
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 
-@app.route("/start/<string:name>")
-def enable(name):
+@app.route("/program")
+def get_program():
+    """
+    Return the name of the program that is currently running
+    
+    :return: the name of the program
+    """
+    program = get_running_program()
+
+    if not program:
+        return "", 204
+
+    return jsonify(program), 200
+
+
+@app.route("/program/<string:name>", methods=['PUT'])
+def set_program(name):
+    """
+    Start execution of the program with given name
+    
+    :param name: name of the program to execute 
+    :return: 
+    """
     global PROGRAM_PROCESS
+    global PROGRAM_PATH
 
     if PROGRAM_PROCESS:
         PROGRAM_PROCESS.terminate()
 
-    program = os.getcwd() + '/programs/' + name + '.py'
+    program = PROGRAM_PATH + name + '.py'
 
     if not os.path.isfile(program):
-        return "program not found", 404
+        return jsonify("program not found"), 404
 
     PROGRAM_PROCESS = Popen(['/usr/local/bin/python3', program])
 
-    return "ok", 200
+    return jsonify(get_running_program()), 200
 
 
-@app.route("/stop")
-
-def disable():
+@app.route("/program", methods=['PUT'])
+def stop_program():
+    """
+    Terminates the program that is currently running
+    
+    :return: 
+    """
     global PROGRAM_PROCESS
     if PROGRAM_PROCESS:
         PROGRAM_PROCESS.terminate()
-    return "disabled", 200
+    return "", 204
+
+
+@app.route("/programs")
+def get_programs():
+    """
+    Returns a list of available programs
+    
+    :return:
+    """
+    global PROGRAM_PATH
+    programs = [x[0:-3] for x in os.listdir(PROGRAM_PATH) if x.endswith('.py')]
+    return jsonify(programs), 200
 
 
 if __name__ == "__main__":
--- programs/test.py
+++ programs/test.py
@@ -5,25 +5,42 @@ from time import sleep
 client = RGBCRt('127.0.0.1', 1337)
 
 
+def set_color(color):
+    client.set_color(Color(color))
+
+
 def transition(color1, color2, time):
-    color1 = Color(color1)
-    color2 = Color(color2)
+    color1, color2 = Color(color1), Color(color2)
 
     for color in color1.range_to(color2, time * 50):
-        client.set_color(color)
+        set_color(color)
         sleep(0.02)
 
 
 def cycle(colors, cycle_time=10, wait=0.1):
-    transition_time = min(int(cycle_time / len(colors)), 1)
+    """
+    Cycles through a list of colors
+    
+    :param colors:      The list of colors to cycle through
+    :param cycle_time:  Time the entire cycle should take
+    :param wait:        Time to wait after each part of the cycle
+    :return: 
+    """
+    transition_time = max(int(cycle_time / len(colors)), 1)
 
     for i, color in enumerate(colors):
         next = (i + 1) % len(colors)
-        print(color, colors[next], i, next)
         transition(color, colors[next], transition_time)
         sleep(wait)
 
 
+def pulse(color, cycle_time=1, pulse_distance=1.2):
+    set_color('black')
+    cycle([Color(color, luminance=0.001), color], cycle_time=cycle_time, wait=0)
+    set_color('black')
+    sleep(pulse_distance)
+
+
 while True:
-    cycle(['blue', 'red', 'green'], 10)
+    pulse('purple', 3)
 
