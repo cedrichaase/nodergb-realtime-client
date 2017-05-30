590c0646d5f20e353f051e8a0b599d775469ad4c
Cedric Haase <cedric@sineband.de>
2017-05-30 13:27:21 +0200

    Mount routes appropriately

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from subprocess import Popen
 
 from flask import Flask
 from flask import jsonify
+from flask import Blueprint
 
 app = Flask(__name__)
+
+ctrl = Blueprint("ctrl", __name__)
+mgmt = Blueprint("mgmt", __name__)
+
 
 PROGRAM_PROCESS = None
 PROGRAM_PATH = os.getcwd() + '/programs/'
@@ -17,12 +22,19 @@ def get_running_program():
     return PROGRAM_PROCESS.args[1].split('/')[-1][:-3] if PROGRAM_PROCESS else ""
 
 
+def stop_running_program():
+    global PROGRAM_PROCESS
+    if PROGRAM_PROCESS:
+        PROGRAM_PROCESS.terminate()
+        PROGRAM_PROCESS = None
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
 
-@app.route("/program")
+@ctrl.route("/program")
 def get_program():
     """
     Return the name of the program that is currently running
@@ -37,7 +49,7 @@ def get_program():
     return jsonify(program), 200
 
 
-@app.route("/program/<string:name>", methods=['PUT'])
+@ctrl.route("/program/<string:name>", methods=['PUT'])
 def set_program(name):
     """
     Start execution of the program with given name
@@ -49,7 +61,7 @@ def set_program(name):
     global PROGRAM_PATH
 
     if PROGRAM_PROCESS:
-        PROGRAM_PROCESS.terminate()
+        stop_running_program()
 
     program = PROGRAM_PATH + name + '.py'
 
@@ -61,7 +73,7 @@ def set_program(name):
     return jsonify(get_running_program()), 200
 
 
-@app.route("/program", methods=['PUT'])
+@ctrl.route("/program", methods=['PUT'])
 def stop_program():
     """
     Terminates the program that is currently running
@@ -69,12 +81,11 @@ def stop_program():
     :return: 
     """
     global PROGRAM_PROCESS
-    if PROGRAM_PROCESS:
-        PROGRAM_PROCESS.terminate()
+    stop_running_program()
     return "", 204
 
 
-@app.route("/programs")
+@mgmt.route("/programs")
 def get_programs():
     """
     Returns a list of available programs
@@ -85,6 +96,9 @@ def get_programs():
     programs = [x[0:-3] for x in os.listdir(PROGRAM_PATH) if x.endswith('.py')]
     return jsonify(programs), 200
 
+
+app.register_blueprint(ctrl, url_prefix="/ctrl")
+app.register_blueprint(mgmt, url_prefix="/mgmt")
 
 if __name__ == "__main__":
     app.run()
